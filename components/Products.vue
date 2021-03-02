0f971f21fea13806c6c9bec3128ebac088da3ded
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-02 13:28:05 +0300

    Added 'Products' component and 'Carousel.client.js' plugin it depends on

---
+++ components/Products.vue
@@ -0,0 +1,90 @@
+<template>
+  <main class="container my-5">
+    <client-only>
+      <carousel-3d :style="{ height: '370px' }" :autoplay="true">
+        <slide
+          v-for="(slide, i) in slides"
+          :key="i"
+          :style="{
+            width: '300px',
+            height: '370px',
+            border: 'none'
+          }"
+          :index="i"
+          class="bg-white shadow-sm"
+        >
+          <div
+            :style="{
+              backgroundImage: `url('${slide.url}')`,
+              width: '100%',
+              height: '370px',
+              backgroundSize: 'auto 330px',
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: '50% 0'
+            }"
+            class="d-flex align-items-end justify-content-center"
+          >
+            <div
+              :style="{ height: '40px' }"
+              class="d-flex justify-content-center w-100 align-items-center"
+            >
+              <p
+                class="text-nowrap m-0 h-100 w-100 d-flex align-items-center justify-content-center"
+              >
+                {{ slide.name }}
+              </p>
+            </div>
+          </div>
+        </slide>
+      </carousel-3d>
+    </client-only>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      slides: [
+        {
+          id: 0,
+          name: "Termal Uzun Kol T-Shirt",
+          url: "https://termalicgiyim.com/img/womenlongsleeve.jpg"
+        },
+        {
+          id: 1,
+          name: "Termal Bere",
+          url: "https://termalicgiyim.com/img/bere.jpg"
+        },
+        {
+          id: 2,
+          name: "Termal Kısa Kol T-Shirt",
+          url: "https://termalicgiyim.com/img/yunkisakol.jpg"
+        },
+        {
+          id: 3,
+          name: "Termal Kar Maskesi",
+          url: "https://termalicgiyim.com/img/karmaskesi.jpg"
+        },
+        {
+          id: 4,
+          name: "Termal Uzun Kol T-Shirt",
+          url: "https://termalicgiyim.com/img/DSC_8694.jpg"
+        },
+        {
+          id: 5,
+          name: "Termal Boyunluk",
+          url: "https://termalicgiyim.com/img/boyunluk.jpg"
+        },
+        {
+          id: 6,
+          name: "Termal Sıfır Kol T-Shirt",
+          url: "https://termalicgiyim.com/img/DSC_8683.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,27 +1,23 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'termalicgiyim',
+    title: "termalicgiyim",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [{ src: "~/plugins/Carousel.client.js", ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -29,16 +25,15 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    "@nuxtjs/eslint-module"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt'
+    "bootstrap-vue/nuxt"
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
