2a49beecec208b47211561646b2e318749aef535
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-03 01:37:18 +0300

    Removes unnecessary style elements

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,28 +18,11 @@ body {
   margin: 0;
 }
 
-.logo img {
-  width: auto;
-  height: 50px;
-}
-
 img {
   border-style: none;
 }
 
-a.nuxt-link-exact-active,
-a {
-  text-decoration: none !important;
-  color: #3a3a3a !important;
-}
-
 small {
   font-size: xx-small !important;
-}
-
-ul {
-  list-style: none;
-  padding: 0 !important;
-  margin: 0 !important;
 }
 </style>
