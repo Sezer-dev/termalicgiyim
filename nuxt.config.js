f42a8210c6da1c1950fe1dfc712c7f3ef460d1ad
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-04 19:03:10 +0300

    Changed meta title and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,22 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "termalicgiyim",
+    title: "Termal İç Giyim",
     htmlAttrs: {
-      lang: "en"
+      lang: "tr"
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "https://termalicgiyim.com/favicon.ico"
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
